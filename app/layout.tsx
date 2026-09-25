57cd4853de943988b03edbbbab642ff7cbdac022
godfredapps <appiahgodfred8@gmail.com>
2026-09-25 13:59:57 +0000

    feat(gtm): add Google Tag Manager (GTM-PXPJZ6XC)

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,6 +84,8 @@ export const metadata: Metadata = {
   },
 }
 
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || 'GTM-PXPJZ6XC'
+
 export default function RootLayout({
   children,
 }: {
@@ -92,6 +94,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+
         {/* Google Analytics */}
         {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
           <>
@@ -206,6 +219,15 @@ export default function RootLayout({
         </Script>
       </head>
       <body className={`${plusJakartaSans.variable} ${archivo.variable} font-sans antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
